ebaca8a25d4e611bdd308be605ff217cddc0261c
nelipovi4 <zhenya.nelipovich@gmail.com>
2025-02-17 23:29:37 +0300

    ОБНОВЛЕНИЕ -Добавлены основыные кнопки -Добавлено уладение всей сроки -Добавление удаление последнего символа -Добавлена основная логика для кнопок

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Data;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,14 +16,47 @@ using System.Windows.Shapes;
 
 namespace KPO_1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach(UIElement el in MainRoot.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "CE")
+            {
+                ResultLabel.Text = "";
+            }
+            else if (str == "=")
+            {
+                string expression = ResultLabel.Text.Replace(",", "."); // Делаем это для устранение ошибок при подсчёте
+                string value = new DataTable().Compute(expression, null).ToString();
+                ResultLabel.Text = value;
+            }
+            else if (str == "del")
+            {
+                if (ResultLabel.Text.Length > 0)
+                {
+                    ResultLabel.Text = ResultLabel.Text.Substring(0, ResultLabel.Text.Length - 1);
+                }
+            }
+            else
+            {
+                ResultLabel.Text += str;
+            }
+
+
         }
     }
 }
